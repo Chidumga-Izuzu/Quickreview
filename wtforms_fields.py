f7a51f81498216b9b2f222ac355548e740e0b390
Dums Izu <dumsizu@dumss-mbp.attlocal.net>
2020-07-13 15:53:27 -0500

    update reg form and validators check

--- wtforms_fields.py
+++ wtforms_fields.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
-
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models import User
 
 class RegistrationForm(FlaskForm):
-    """ Registration Form """
 
+    """ Registration Form """
 
     username = StringField('username_l', validators=[InputRequired(message="Please enter username"), Length(min=5, max=20, message= "Username must be between 5 and 20 characters")])
     password = PasswordField('password_l', validators=[InputRequired(message="password required"), Length(min=5, max=10, message= "Password must be between 5 and 10 characters")])
     confirm_password = PasswordField('confirm_password_l', validators=[InputRequired(message="Confirm password"), EqualTo('password', message="Passwords must match ")])
     submit_button = SubmitField ('Create Account')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Please select a different username.")
