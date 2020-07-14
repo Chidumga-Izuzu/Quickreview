05c06cea52f70ebb51fca1d95227b066a5ad3d84
Dums Izu <dumsizu@dumss-mbp.attlocal.net>
2020-07-14 00:20:13 -0500

    hash password for security

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
+from passlib.hash import pbkdf2_sha256
+
 from wtforms_fields import *
 from models import *
 
@@ -22,8 +24,12 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+        #hash password
+
+        hashed_pass = pbkdf2_sha256.hash(password)
+
             #Add user to database
-        user = User(username=username, password=password)
+        user = User(username=username, password=hashed_pass)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -43,7 +49,7 @@ def login():
 
     return render_template("login.html", form=login_form)
 
-            
+
 
 
 if __name__ == "__main__":
