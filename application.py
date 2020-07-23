1bedd91609a6d5ab2a1a7e39c2099e61165691d1
Dums Izu <dumsizu@dumss-mbp.attlocal.net>
2020-07-23 18:00:45 -0500

    completed backend

--- application.py
+++ application.py
@@ -1,7 +1,14 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for
+import requests
+from flask import Flask, session, request, logging, render_template, redirect, url_for
+from flask import json, jsonify
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from sqlalchemy import or_, and_
+from sqlalchemy import join
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.session import sessionmaker
 from wtforms_fields import *
 from models import *
 
@@ -15,6 +22,11 @@ app.secret_key = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://lhqkptsjnyzkik:f8cf13cdc00796fd789d6f2cc514f88936d2620130eaeb7cecbcd3f10fd6f250@ec2-34-239-241-25.compute-1.amazonaws.com:5432/daitbrcvb621sd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
 
 #configure flask login
 
@@ -31,6 +43,7 @@ def index():
     reg_form = RegistrationForm()
 
     if reg_form.validate_on_submit():
+
         username = reg_form.username.data
         password = reg_form.password.data
 
@@ -58,7 +71,7 @@ def login():
         user_object=User.query.filter_by(username=login_form.username.data).first()
         login_user(user_object)
         if current_user.is_authenticated:
-            return "logged in with flask-login"
+            return redirect(url_for('search'))
         return "not logged in :("
         #return redirect(url_for( 'search'))
     return render_template("login.html", form=login_form)
@@ -69,21 +82,90 @@ def search():
     if request.method == "GET":
         return render_template("search.html")
     else:
-        isbn = request.form.get("isbn")
-        title =  request.form.get("title")
-        author =  request.form.get("author")
+         isbn = request.form.get("isbn")
+         title =  request.form.get("title")
+         author =  request.form.get("author")
 
-        books = Book.query.filter(or_ (Book.author.ilike(author), Book.isbn.ilike(isbn), Book.title.ilike(title)) ).all()
-
-        #db.engine.execute("SELECT * FROM books WHERE isbn LIKE :isbn AND title LIKE :title AND author LIKE :author",
-                            #{"isbn": isbn, "title": title, "author": author}).fetchall()
-        if len(books) == 0:
+         books = Book.query.filter(or_ (Book.author.ilike(author), Book.isbn.ilike(isbn), Book.title.ilike(title)) ).all()
+         if len(books) == 0:
             return render_template("search.html")
 
-        return render_template("result.html", books=books)
+         return render_template("result.html", books=books)
 
 
-    #return "Hey! search for books..."
+#Get book info
+@app.route('/book/<isbn>', methods=["GET","POST"])
+@login_required
+def book(isbn):
+
+
+    book = Book.query.get(isbn)
+    if book is None:
+        return render_template("error.html", message="Book not found"), 404
+
+    if request.method == "POST":
+        if "add" in request.form or "update" in request.form:
+            comment = request.form.get("comment")
+            rating = int(request.form.get("rating"))
+
+            if (rating < 1 & rating > 5):
+                return render_template("error.html", message="incorrect value")
+
+            review_check = Review.query.filter(and_(Review.isbn==isbn, Review.username==current_user.username)).first()
+            if review_check is None:
+                review_add = Review(username=current_user.username, isbn=isbn, review=comment, review_numb=rating)
+                db.session.add(review_add)
+
+            else:
+                review_check.review = comment
+                review_check.review_numb = rating
+            db.session.commit()
+
+        elif "delete" in request.form:
+            review_check = Review.query.filter(and_(Review.isbn==isbn, Review.username==current_user.username)).first()
+            db.session.delete(review_check)
+            db.session.commit()
+
+    review_check = Review.query.filter(Review.isbn==isbn).all()
+    user_review = [comment for comment in review_check if comment.username == current_user.username]
+    if user_review:
+        user_review=user_review[0]
+        review_check.remove(user_review)
+    # Get data from Goodreads API
+    ### Change key to your goodreads API key below ###
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "KGJIBaI7CKEgWvsELMOITA", "isbns": book.isbn})
+
+
+    if res.status_code == 200:
+        goodreadsResult = res.json()
+        return render_template("results.html", book=book, goodreadsResult=goodreadsResult, review_check=review_check, user_review=user_review)
+    return render_template("results.html", book=book, review_check=review_check, user_review=user_review)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/api/<string:isbn>")
+def api(isbn):
+    # Make sure isbn exists
+    book = Book.query.filter_by(isbn=isbn).first()
+    if book is None:
+        return render_template("error.html", message="404 Book not found"), 404
+    return render_template("api.html", book=book)
+
+
+
+
+
 
 @app.route("/logout", methods=['GET'])
 def logout():
--- models.py
+++ models.py
@@ -11,23 +11,56 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def get_id(self):
+        return self.id
+
 class Book(db.Model):
-    """ Book model """
+    """ books model """
 
     __tablename__ = "books"
-    id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String(), nullable=False)
+    isbn = db.Column(db.String(), primary_key=True)
     title = db.Column(db.String(), nullable=False)
     author = db.Column(db.String(), nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
+# Returns information about the book in JSON format
+    def toJson(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "isbn": self.isbn,
+            "review_count": self.getReviewsnumb(),
+            "average_score": self.getReviewsAvg(),
+        }
+
+    def getReviewsnumb(self):
+        return len(Review.query.filter_by(isbn=self.isbn).all())
+
+    # Get average review rating for the book (returns 0.0 if no reviews)
+    def getReviewsAvg(self):
+        reviewer = Review.query.filter_by(isbn=self.book_id).with_entities(Review.review_numb).all()
+        reviewer = [r for r, in reviewer]
+        if reviewer:
+            return sum(reviewer) / len(reviewer)
+        else:
+            return 0.0
+
 
 class Review(db.Model):
-    """ Review model """
+    """ reviews model """
 
     __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(25), db.ForeignKey('users.username'), nullable=False)
+    isbn = db.Column(db.String(), db.ForeignKey('books.isbn'), nullable=False)
     review = db.Column(db.String(), nullable=False)
+    review_numb = db.Column(db.SmallInteger, unique=False, nullable=False)
+
+
+    def getUsername(self):
+        return User.query.filter_by(id=self.id).first().username
